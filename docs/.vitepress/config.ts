0180f0789f3b52732ce91f636ecaf8102efcb2cd
lvliqi <lvliqi_d@163.com>
2023-05-02 19:55:03 +0800

    feat: 修改文档构建参数

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,6 @@ import pkg from '../../package.json';
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: `/${pkg.name}/docs/.vitepress/dist`,
-  title: pkg.name,
+  title: `${pkg.name}-${pkg.version}`,
   description: pkg.description
 })
