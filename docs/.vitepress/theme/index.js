c9de00ee0fc5a6435b7d5442c8d31deae45ad968
lvliqi <lvliqi_d@163.com>
2023-05-02 15:39:04 +0800

    feat: 修改doc

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,10 +2,11 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import './custom.css';
 import Layout from './layout.vue'
+import NotFound from './not-found.vue'
 
 export default {
   Layout,
-  NotFound: () => 'custom 404',
+  NotFound,
   enhanceApp({ app, router, siteData }) {
     app.use(ElementPlus);
   }
