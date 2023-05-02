b6a74484ee476767e8531c2cf46b755babb352d1
lvliqi <lvliqi_d@163.com>
2023-05-02 20:38:09 +0800

    feat: 修改文档配置

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import * as path from 'path';
+import pkg from '../package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
     alias: {
       '@components': path.resolve(__dirname, '.vitepress/components'),
       '@': path.resolve(__dirname, '../src'),
+      [pkg.name]: path.resolve(__dirname, '../src'),
       '@dist': path.resolve(__dirname, '../')
     }
   },
