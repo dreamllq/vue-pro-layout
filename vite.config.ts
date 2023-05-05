c270277162e3fc6ce9f10a1f131d360c15dfe947
lvliqi <lvliqi_d@163.com>
2023-05-05 23:09:00 +0800

    feat: 修改构建文件

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     lib: {
       entry: 'src/index.ts',
       name: 'ViteVue3TsComponentTemplate',
-      fileName: 'vite-vue3-ts-component-template'
+      fileName: 'index'
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
